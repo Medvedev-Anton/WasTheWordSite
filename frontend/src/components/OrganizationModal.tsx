fa7c2a81dba09b3f37a237faf3538f0bf4c62299
Medvedev Anton <laveyant@yandex.ru>
2026-03-28 08:25:33 +0300

    feat: add Volunteer/Sports org types, change Svobodnaya suborg to Group, larger covers on desktop

--- frontend/src/components/OrganizationModal.tsx
+++ frontend/src/components/OrganizationModal.tsx
@@ -33,6 +33,8 @@ export default function OrganizationModal({
         'Коммерческая': '/image/organizations/commercial.jpg',
         'Административная': '/image/organizations/administrative.jpg',
         'Образовательная': '/image/organizations/educational.jpg',
+        'Волонтёрская': '/image/organizations/free_appointment.jpg',
+        'Спортивная': '/image/organizations/free_appointment.jpg',
         'Свободная': '/image/organizations/free_appointment.jpg'
     };
 
