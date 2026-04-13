e20ce27ef5cd26d24085c5f358bf67b1cea588ef
reightenfels <reightenfels@mail.ru>
2026-04-13 15:07:26 +0300

    add: user rang service

---
+++ backend/services/user_rang/user_rang_service.js
@@ -0,0 +1,16 @@
+import { UserRangServiceInterface } from "./user_rang_service_interface";
+
+export class UserRangService extends UserRangServiceInterface {
+    constructor(mapper) {
+        super(mapper);
+    }
+
+    getRang(userId) {
+        try {
+            return this.mapper.getRang(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
---
+++ backend/services/user_rang/user_rang_service_interface.js
@@ -0,0 +1,22 @@
+import { UserRangMapperInterface } from "../../mappers/user_rang/user_rang_mapper_interface.js";
+
+export class UserRangServiceInterface {
+    /**
+     * @param {UserRangMapperInterface} mapper 
+     */
+    constructor(mapper) {
+        if (new.target === 'UserRangServiceInterface') {
+            throw new Error('Нельзя создать экземпляр класса UserRangServiceInterface');
+        }
+
+        this.mapper = mapper;
+    }
+
+    /**
+     * Возвращает ранг пользователя
+     * @param {int} userId 
+     */
+    getRang(userId) {
+        throw new Error('getRang должен быть переопределен в наследнике');
+    }
+}
