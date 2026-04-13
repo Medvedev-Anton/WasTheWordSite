a21e5de3049aabc4817783198ffb2f1a1be67769
reightenfels <reightenfels@mail.ru>
2026-04-13 15:53:38 +0300

    add: rang posts resolver structure

--- backend/services/rang_resolver/strategies/post_resolver.js
+++ backend/services/rang_resolver/strategies/post_resolver.js
@@ -1,3 +1,5 @@
+import { PostsFacade } from "../../../facades/posts_facade.js";
+import { RangFacade } from "../../../facades/rang_facade.js";
 import { RangResolveStrategy } from "./rang_resolve_strategy.js";
 
 /**
@@ -5,6 +7,42 @@ import { RangResolveStrategy } from "./rang_resolve_strategy.js";
  */
 export class PostResolver extends RangResolveStrategy {
     resolve(userId) {
-        return 0;
+        const minOrderNumber = 1;
+        const maxOrderNumber = 5;
+
+        let currentUserRang = -1;
+
+        try {
+            currentUserRang = RangFacade.getUserRangId(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+
+        if (currentUserRang === -1) {
+            return -1;
+        }
+
+        if (currentUserRang >= maxOrderNumber) {
+            return currentUserRang;
+        }
+
+        const countUserPosts = PostsFacade.getTotalCountByUser(userId);
+
+        if (typeof countUserPosts !== 'number') {
+            return currentUserRang;
+        }
+
+        if (countUserPosts === 1) {
+            
+        }
+
+        /**
+         * 1) Получить текущий ранг пользователя
+         * 2) Если текущий ранг больше maxOrderNumber, то возвращает его. Иначе идем далее
+         * 3) Получаем количество созданных пользователем постов
+         * 4) Вычисляем ранг в зависимости от количества постов
+         * 5) Возвращаем вычисленный ранг
+         */
     }
 }
