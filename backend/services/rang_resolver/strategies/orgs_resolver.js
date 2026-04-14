2bc63221b342d3bcab437b6e590d34d8a2469af2
reightenfels <reightenfels@mail.ru>
2026-04-14 10:20:27 +0300

    add: orgs rang resolver strategy

--- backend/services/rang_resolver/strategies/orgs_resolver.js
+++ backend/services/rang_resolver/strategies/orgs_resolver.js
@@ -1,10 +1,82 @@
 import { RangResolveStrategy } from "./rang_resolve_strategy.js";
+import { RangFacade } from "../../../facades/rang_facade.js";
+import { OrgsFacade } from "../../../facades/orgs_facade.js";
 
 /**
  * Реализует логику вычисления ранга при создании организаций
  */
 export class OrgsResolver extends RangResolveStrategy {
     resolve(userId) {
-        return 0;
+        const maxOrderNumber = 20;
+
+        let currentUserRangId = -1;
+
+        try {
+            currentUserRangId = RangFacade.getUserRangId(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+
+        if (currentUserRangId === -1) {
+            return -1;
+        }
+
+        let currentUserRangOrderNumber = 0;
+        
+        try {
+            const rang = RangFacade.findById(currentUserRangId);
+            currentUserRangOrderNumber = rang.getOrderNumber();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+
+        if (typeof currentUserRangOrderNumber !== 'number' || currentUserRangOrderNumber < 0) {
+            return currentUserRangId;
+        }
+
+        if (currentUserRangOrderNumber > maxOrderNumber) {
+            return currentUserRangId;
+        }
+
+        const countUserOrgs = OrgsFacade.getTotalCountByUser(userId);
+
+        if (typeof countUserOrgs !== 'number') {
+            return currentUserRang;
+        }
+
+        try {
+            if (countUserOrgs < 1) {
+                return RangFacade.findByOrderNumber(13).getId();
+            }
+            else if (countUserOrgs == 1) {
+                return RangFacade.findByOrderNumber(14).getId();
+            }
+            else if (countUserOrgs == 2) {
+                return RangFacade.findByOrderNumber(15).getId();
+            }
+            else if (countUserOrgs == 3) {
+                return RangFacade.findByOrderNumber(16).getId();
+            }
+            else if (countUserOrgs == 4) {
+                return RangFacade.findByOrderNumber(17).getId();
+            }
+            else if (countUserOrgs == 5) {
+                return RangFacade.findByOrderNumber(18).getId();
+            }
+            else if (countUserOrgs == 6) {
+                return RangFacade.findByOrderNumber(19).getId();
+            }
+            else if (countUserOrgs > 6 && countUserOrgs <= 10) {
+                return RangFacade.findByOrderNumber(20).getId();
+            }
+            else if (countUserOrgs > 10) {
+                return RangFacade.findByOrderNumber(20).getId();
+            }
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
     }
 }
