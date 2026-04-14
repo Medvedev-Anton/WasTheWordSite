ec887cd50ca48ac592d1d67c300d2c41ff96b570
reightenfels <reightenfels@mail.ru>
2026-04-14 12:18:57 +0300

    add: update user ran on suborgs

--- backend/services/rang_resolver/strategies/suborgs_resolver.js
+++ backend/services/rang_resolver/strategies/suborgs_resolver.js
@@ -1,10 +1,82 @@
 import { RangResolveStrategy } from "./rang_resolve_strategy.js";
+import { RangFacade } from "../../../facades/rang_facade.js";
+import { OrgsFacade } from "../../../facades/orgs_facade.js";
+import { UserFacade } from "../../../facades/user_facade.js";
 
 /**
- * Реализует логику вычисления ранга при создании подорганизаций
+ * Реализует логику вычисления ранга для количества подорганизаций
  */
 export class SuborgsResolver extends RangResolveStrategy {
     resolve(userId) {
-        return 0;
+        const maxOrderNumber = 13;
+        
+        let currentUserRangId = -1;
+
+        try {
+            currentUserRangId = RangFacade.getUserRangId(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+
+        if (currentUserRangId === -1) {
+            return -1;
+        }
+
+        let currentUserRangOrderNumber = 0;
+        
+        try {
+            const rang = RangFacade.findById(currentUserRangId);
+            currentUserRangOrderNumber = rang.getOrderNumber();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+
+        if (typeof currentUserRangOrderNumber !== 'number' || currentUserRangOrderNumber < 0) {
+            return currentUserRangId;
+        }
+
+        if (currentUserRangOrderNumber > maxOrderNumber) {
+            return currentUserRangId;
+        }
+
+        if (currentUserRangOrderNumber <= 10) {
+            const countSuborgs = OrgsFacade.getMaxCountSuborgsForSuborgsByUser(userId);
+
+            if (countSuborgs < 1) {
+                UserFacade.calcAndUpdateRang(userId, 'posts');
+            }
+
+            const countAndRangMapperObj = {
+                1: 6,
+                2: 7,
+                3: 8,
+                4: 9,
+                5: 10
+            };
+
+            const newRangNumber = countAndRangMapperObj[countSuborgs];
+
+            if (newRangNumber !== undefined) {
+                return RangFacade.findByOrderNumber(newRangNumber).getId();
+            }            
+            else {
+                return RangFacade.findByOrderNumber(10).getId();
+            }
+        }
+        else {
+            const countSuborgs = OrgsFacade.getMaxCountSuborgsForOrgsByUser(userId);
+
+            const countAndRangMapperObj = {
+                1: 11,
+                2: 12,
+                3: 13,
+            };
+
+            const newRangNumber = countAndRangMapperObj[countSuborgs];
+
+            return RangFacade.findByOrderNumber(newRangNumber).getId();
+        }
     }
 }
