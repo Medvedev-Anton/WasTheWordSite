8a323883b68f36e4ed98aca8bbf535c61446c909
reightenfels <reightenfels@mail.ru>
2026-05-07 13:29:55 +0300

    add: initial balances mapper

---
+++ backend/mappers/intital_balances/intital_balances_mapper.js
@@ -0,0 +1,38 @@
+import { InitialBalancesMapperInterface } from "./intital_balances_mapper_interface.js";
+import { db } from "../../database/init.js";
+
+export class InitialBalancesMapper extends InitialBalancesMapperInterface {
+    constructor() {
+        super();
+    }
+
+    getBalanceByName(name) {
+        const result = db.prepare(`
+            SELECT
+                value
+            FROM initial_balances
+            WHERE
+                name = ?    
+        `).get(name);
+
+        if (!result) {
+            return 0;
+        }
+
+        return parseInt(result.value);
+    }
+
+    updateBalanceByName(name, balance) {
+        if (balance < 0) {
+            throw new Error('начальный баланс не может быть отрпицательным');
+        }
+
+        const result = db.prepare(`
+            UPDATE
+                initial_balances
+            SET value = ?
+            WHERE
+                name = ?    
+        `).run(name, balance);
+    }
+}
