40e7d7fe67fcf763435a773adf9255172c63e191
reightenfels <reightenfels@mail.ru>
2026-05-12 15:46:30 +0300

    add: prices facade

---
+++ backend/facades/prices_facade.js
@@ -0,0 +1,58 @@
+import { PricesMapper } from "../mappers/prices/prices_mapper.js";
+import { PricesService } from "../services/prices/prices_service.js";
+
+export class PricesFacade {
+    static getService() {
+        return new PricesService(
+            new PricesMapper()
+        );
+    }
+
+    /**
+     * Получает цену за просмотр поста
+     */
+    getPostViewPrice() {
+        try {
+            return this.getService().getPostViewPrice();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Получает цену за создание организации
+     */
+    getOrgCreatePrice() {
+        try {
+            return this.getService().getOrgCreatePrice();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновляет цену за создание поста
+     */
+    updatePostViewPrice(newPrice) {
+        try {
+            return this.getService().updatePostViewPrice(newPrice);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновляет цену за создание организации
+     */
+    updateOrgCreatePrice(newPrice) {
+        try {
+            return this.getService().updateOrgCreatePrice(newPrice);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
