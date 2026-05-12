6eb72dded653562cedcb59749f2ff13fd217f48c
reightenfels <reightenfels@mail.ru>
2026-05-12 16:36:03 +0300

    add: profit facade

---
+++ backend/facades/profit_facade.js
@@ -0,0 +1,57 @@
+import { ProfitOrgsMapper } from "../mappers/profit/profit_orgs_mapper.js";
+import { ProfitUsersMapper } from "../mappers/profit/profit_users_mapper.js";
+import { ProfitService } from "../services/profit/profit_service.js";
+
+export class ProfitFacade {
+    constructor(entity) {
+        if (entity === 'users') {
+            this.service = new ProfitService(
+                new ProfitUsersMapper()
+            );
+        }
+        else if (entity === 'orgs') {
+            this.service = new ProfitService(
+                new ProfitOrgsMapper()
+            );
+        }
+        else {
+            throw new Error('Неизвестная сущность для вычисления дохода: ' + entity);
+        }
+    }
+
+    static entity(entity) {
+        return new self(entity);
+    }
+
+    /**
+     * Создает запись о доходе
+     * @param {int} entityId
+     * @param {int} incomingSum
+     * @param {string} date
+     * @return {int}
+     */
+    createProfit(entityId, incomingSum, date) {
+        try {
+            return this.service.create(entityId, incomingSum, date);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Возвращает сумму доходов сущности за указанный период
+     * @param {int} entityId
+     * @param {string} dateStart
+     * @param {string} dateFinish
+     * @return {int}
+     */
+    getSumInDateInterval(entityId, dateStart, dateFinish) {
+        try {
+            return this.service.getSumInDateInterval(entityId, dateStart, dateFinish);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
