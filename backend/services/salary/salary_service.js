b434789a1e92b9d3b592fe9fc92debb05bc8ed3b
reightenfels <reightenfels@mail.ru>
2026-05-28 11:27:20 +0300

    add: salary service

---
+++ backend/services/salary/salary_service.js
@@ -0,0 +1,34 @@
+import { SalaryServiceInterface } from "./salary_service_interface.js";
+
+export class SalaryService extends SalaryServiceInterface {
+    constructor(mapper) {
+        super(mapper);
+    }
+
+    getUserSalaryies(userId) {
+        try {
+            return this.mapper.getUserSalaryies(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    create(userId, orgId, salary, payday) {
+        try {
+            return this.mapper.create(userId, orgId, salary, payday);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    delete(userId, orgId) {
+        try {
+            return this.mapper.delete(userId, orgId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
---
+++ backend/services/salary/salary_service_interface.js
@@ -0,0 +1,42 @@
+import { SalaryMapperInterface } from "../../mappers/salary/salary_mapper_interface.js";
+
+export class SalaryServiceInterface {
+    /**
+     * @param {SalaryMapperInterface} mapper 
+     */
+    constructor(mapper) {
+        if (new.target === 'SalaryServiceInterface') {
+            throw new Error('Нельзя создать экземпляр класса SalaryServiceInterface');
+        }
+
+        this.mapper = mapper;
+    }
+
+    /**
+     * Получает зарплаты пользователя
+     * @param {number} userId
+     */
+    getUserSalaryies(userId) {
+        throw new Error('getUserSalaryies должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создает строку с зарплатой пользователя
+     * @param {number} userId
+     * @param {number} orgId
+     * @param {number} salary
+     * @param {string} payday
+     */
+    create(userId, orgId, salary, payday) {
+        throw new Error('create должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаляет строку с зарплатой пользователя
+     * @param {number} userId
+     * @param {number} orgId
+     */
+    delete(userId, orgId) {
+        throw new Error('delete должен быть переопределен в наследнике');
+    }
+}
