27e258c421d9855e9fd11c92a708279505a5cfa6
reightenfels <reightenfels@mail.ru>
2026-06-01 20:50:16 +0300

    add: notifications controller

---
+++ backend/controllers/notifications_controller.js
@@ -0,0 +1,29 @@
+import NotificationsFacade from "../facades/notifications_facade.js";
+import { MainController } from "./main_controller.js";
+
+export default class NotificationsController extends MainController {
+    constructor(request, response) {
+        super(request, response);
+    }
+
+    /**
+     * Обработчик получения всех уведомлений пользователя
+     */
+    getAllUserNotifications() {
+        try {
+            const user = parseInt(this.request.user.userId);
+            const notifications = NotificationsFacade.getAllByUser(user);
+            const notificationsMessages = notifications.map(notify => notify.message);
+
+            this.message(200, {
+                notifications: notificationsMessages
+            });
+        }
+        catch (e) {
+            throw new Error('Get all user notifications error: ' + e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+}
