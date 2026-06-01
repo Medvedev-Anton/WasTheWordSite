266645d0ec3926e0b562c5e18227dbd70b589e11
reightenfels <reightenfels@mail.ru>
2026-06-01 21:12:43 +0300

    add: get notifications in profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -21,6 +21,25 @@ export default function Profile() {
   const [avatarFile, setAvatarFile] = useState<File | null>(null);
   const [photoFile, setPhotoFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(true);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  useEffect(() => {
+    if (notifications.length > 0) {
+      notifications.forEach(notify => {
+        alert(notify);
+      });
+    }
+  }, [notifications]);
+
+  // Запрос на получение всех уведомлений пользовтеля
+  const fetchNotifications = async () => {
+    const result = await axios.get('/api/notifications');
+    setNotifications(result.data.notifications);
+  }
 
   useEffect(() => {
     if (currentUser) {
