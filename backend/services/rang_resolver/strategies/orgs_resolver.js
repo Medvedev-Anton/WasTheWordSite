6862de467ef2f70e0aefb054d54991309c2bf7ef
reightenfels <reightenfels@mail.ru>
2026-06-19 15:11:20 +0300

    fix: rang resolve

--- backend/services/rang_resolver/strategies/orgs_resolver.js
+++ backend/services/rang_resolver/strategies/orgs_resolver.js
@@ -77,16 +77,16 @@ export class OrgsResolver extends RangResolveStrategy {
             else if (countUserOrgs >= 6 && countUserOrgs < 10) {
                 return RangFacade.findByOrderNumber(19).getId();
             }
-            else if (countUserOrgs >= 10 && countUserOrgs <= 15) {
+            else if (countUserOrgs >= 10 && countUserOrgs < 15) {
                 return RangFacade.findByOrderNumber(20).getId();
             }
-            else if (countUserOrgs > 15 && countUserOrgs <= 20) {
+            else if (countUserOrgs >= 15 && countUserOrgs < 20) {
                 return RangFacade.findByOrderNumber(21).getId();
             }
-            else if (countUserOrgs > 20 && countUserOrgs <= 25) {
+            else if (countUserOrgs >= 20 && countUserOrgs < 25) {
                 return RangFacade.findByOrderNumber(22).getId();
             }
-            else if (countUserOrgs > 20) {
+            else if (countUserOrgs >= 25) {
                 return RangFacade.findByOrderNumber(23).getId();
             }
         }
