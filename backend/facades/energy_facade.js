35996d25130b80da184d61d29d5b45dde70e667d
reightenfels <reightenfels@mail.ru>
2026-06-20 22:33:12 +0300

    add: energy facade

---
+++ backend/facades/energy_facade.js
@@ -0,0 +1,37 @@
+import EnergyMapper from "../mappers/energy/energy_mapper.js";
+import EnergyService from "../services/energy/energy_service.js";
+
+export default class EnergyFacade {
+    static getService() {
+        return new EnergyService(
+            new EnergyMapper()
+        );
+    }
+
+    /**
+     * Инкрементирует значение энергии пользователя
+     * @param {number} userId
+     * @param {number} incrementValue
+     */
+    static incrementUser(userId, incrementValue) {
+        try {
+            return this.getService().incrementUser(userId, incrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Получает значение энергии пользователя
+     * @param {number} userId
+     */
+    getByUser(userId) {
+        try {
+            return this.getService().getByUser(userId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
