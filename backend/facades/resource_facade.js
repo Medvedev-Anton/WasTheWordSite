147b3a7a9ede9f9dfb2bf3914269250b40eade12
reightenfels <reightenfels@mail.ru>
2026-07-03 14:09:23 +0300

    add: resources facade

---
+++ backend/facades/resource_facade.js
@@ -0,0 +1,107 @@
+import ResourcesMapper from "../mappers/resources/resources_mapper.js";
+import ResourcesService from "../services/resources/resources_service.js";
+
+export default class ResourceFacade {
+    static getService() {
+        return new ResourcesService(
+            new ResourcesMapper()
+        )
+    }
+
+    /**
+     * Возвращает все ресурсы
+     */
+    static getAll() {
+        try {
+            return this.getService().getAll();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Создание ресурса
+     * @param {string} name
+     * @param {string} imageUrl
+     * @param {number} countNeedEnergy
+     * @param {number} countNeedMoney
+     */
+    static create(name, imageUrl, countNeedEnergy, countNeedMoney) {
+        try {
+            return this.getService().create(name, imageUrl, countNeedEnergy, countNeedMoney);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Удаление ресурса
+     * @param {number} id
+     */
+    static delete(id) {
+        try {
+            return this.getService().delete(id);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновление названия
+     * @param {number} id
+     * @param {string} newName
+     */
+    static updateName(id, newName) {
+        try {
+            return this.getService().updateName(id, newName);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновление изображения
+     * @param {number} id
+     * @param {string} newImageUrl
+     */
+    static updateImageUrl(id, newImageUrl) {
+        try {
+            return this.getService().updateImageUrl(id, newImageUrl);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновление требуемого количества энергии
+     * @param {number} id
+     * @param {number} newEnergy
+     */
+    static updateNeedEnergy(id, newEnergy) {
+        try {
+            return this.getService().updateNeedEnergy(id, newEnergy);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Обновление требуемого количества денег
+     * @param {number} id
+     * @param {number} newMoney
+     */
+    static updateNeedMoney(id, newMoney) {
+        try {
+            return this.getService().updateNeedMoney(id, newMoney);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
