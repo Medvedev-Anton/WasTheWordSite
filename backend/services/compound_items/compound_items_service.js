e9572f23a7b383fd42397ca061f132f964b381a9
reightenfels <reightenfels@mail.ru>
2026-07-04 18:06:46 +0300

    add: compound items service

---
+++ backend/services/compound_items/compound_items_service.js
@@ -0,0 +1,116 @@
+import CompoundItemsServiceInterface from "./compound_items_service_interface.js";
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+
+export default class CompoundItemsService extends CompoundItemsServiceInterface {
+    constructor(mapper) {
+        super(mapper);
+    }
+
+    getAll() {
+        try {
+            return this.mapper.findAll();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    create(number, name, imageUrl, number, itemsParts) {
+        try {
+            return this.mapper.create(number, name, imageUrl, number, itemsParts);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    delete(id) {
+        try {
+            this.deleteImage(id);
+            return this.mapper.delete(id);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    updateNumber(id, newNumber) {
+        try {
+            return this.mapper.update(id, 'number', newNumber);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    updateName(id, newName) {
+        try {
+            return this.mapper.update(id, 'name', newName);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    updateImageUrl(id, newImageUrl) {
+        try {
+            return this.mapper.update(id, 'imageUrl', newImageUrl);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createPart(compoundItemId, partItemId) {
+        try {
+            return this.mapper.createPart(compoundItemId, partItemId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    deletePart(partId) {
+        try {
+            return this.mapper.deletePart(partId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findById(id) {
+        try {
+            return this.mapper.findById(id);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    deleteImage(id) {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+
+        try {
+            const item = this.getById(id);
+
+            if (item === null) {
+                throw new Error('Не найден составной предмет с ID: ' + id);
+            }
+
+            const imageUrl = item.imageUrl;
+
+            const filePath = path.join(__dirname, '../..', imageUrl.replace(/^\//, ''));
+            
+            if (fs.existsSync(filePath)) {
+                try { fs.unlinkSync(filePath); } catch (e) { /* ignore */ }
+            }
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
---
+++ backend/services/compound_items/compound_items_service_interface.js
@@ -0,0 +1,93 @@
+import CompoundItemsMapperInterface from "../../mappers/compound_items/compound_items_mapper_interface.js";
+
+export default class CompoundItemsServiceInterface {
+    /**
+     * @param {CompoundItemsMapperInterface} mapper 
+     */
+    constructor(mapper) { 
+        if (new.target === 'CompoundItemsServiceInterface') {
+            throw new Error('Нельзя создать экземпляр класса CompoundItemsServiceInterface');
+        }
+
+        this.mapper = mapper;
+    }
+
+    /**
+     * Возвращает все предметы
+     */
+    getAll() {
+        throw new Error('getAll должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создание предмета
+     * @param {number} id
+     * @param {string} name
+     * @param {string} imageUrl
+     * @param {number} number
+     * @param {Array<Record<string, number>>} itemsParts
+     */
+    create(number, name, imageUrl, number, itemsParts) {
+        throw new Error('create должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление предмета
+     * @param {number} id
+     */
+    delete(id) {
+        throw new Error('delete должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Обновление номера предмета
+     * @param {number} id
+     * @param {number} newNumber
+     */
+    updateNumber(id, newNumber) {
+        throw new Error('updateNumber должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Обновление названия
+     * @param {number} id
+     * @param {string} newName
+     */
+    updateName(id, newName) {
+        throw new Error('updateName должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Обновление изображения
+     * @param {number} id
+     * @param {string} newImageUrl
+     */
+    updateImageUrl(id, newImageUrl) {
+        throw new Error('updateName должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создание части предмета
+     * @param {number} compoundItemId
+     * @param {number} partItemId
+     */
+    createPart(compoundItemId, partItemId) {
+        throw new Error('createPart должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление части предмета
+     * @param {number} partId
+     */
+    deletePart(partId) {
+        throw new Error('deletePart должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Возвращает данные предмета 
+     * @param {number} id
+     */
+    findById(id) {
+        throw new Error('findById должен быть переопределен в наследнике');
+    }
+}
