5e0ec858714038409cb50c8940d15d52b261b77a
reightenfels <reightenfels@mail.ru>
2026-07-04 16:01:10 +0300

    add: simple items controller

---
+++ backend/controllers/simple_items_controller.js
@@ -0,0 +1,332 @@
+import SimpleItemsFacade from "../facades/simple_items_facade.js";
+import { MainController } from "./main_controller.js";
+
+export default class SimpleItemsController extends MainController {
+    constructor (request, response) {
+        super(request, response);
+    }
+
+    /**
+     * Возвращает URL загруженного изображения
+     */
+    getUploadedFileUrl(fieldName) {
+        const files = this.request.files;
+        if (!files || typeof files !== 'object') {
+            return null;
+        }
+
+        const uploaded = files[fieldName]?.[0];
+        return uploaded ? `/uploads/simple-items/${uploaded.filename}` : null;
+    }
+
+    /**
+     * Обработчик запроса на получение всех предметов
+     */
+    getAll() {
+        try {
+            const items = SimpleItemsFacade.getAll();
+
+            this.send(200, {
+                items: items
+            });
+        }
+        catch (e) {
+            console.error('Get all items error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Создание предмета
+     */
+    create() {
+        const validate = this.has([
+            'number',
+            'name', 
+            'countNeedEnergy', 
+            'countNeedMoney',
+            'needResourceId',
+            'countNeedResource'
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const {
+                number, 
+                name, 
+                countNeedEnergy, 
+                countNeedMoney,
+                needResourceId,
+                countNeedResource
+            } = this.request.body;
+
+            const imageUrl = this.getUploadedFileUrl('image');
+
+            SimpleItemsFacade.create(number, name, imageUrl, countNeedEnergy, countNeedMoney, needResourceId, countNeedResource);
+            
+            const items = SimpleItemsFacade.getAll();
+
+            this.send(201, {
+                items: items
+            });
+        }
+        catch (e) {
+            console.error('Create item error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Удаление предмета
+     */
+    delete() {
+        const validate = this.has([
+            'id', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+
+            SimpleItemsFacade.delete(id);
+
+            const items = SimpleItemsFacade.getAll();
+
+            this.send(200, {
+                message: 'Success',
+                items: items
+            });
+        }
+        catch (e) {
+            console.error('Delete item error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление номера
+     */
+    updateNumber() {
+        const validate = this.has([
+            'id',
+            'number', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const number = parseInt(this.request.body.number);
+
+            SimpleItemsFacade.updateNumber(id, number);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item number error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление имени
+     */
+    updateName() {
+        const validate = this.has([
+            'id',
+            'name', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const name = this.request.body.name;
+
+            SimpleItemsFacade.updateName(id, name);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item name error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление изображения
+     */
+    updateImage() {
+         const validate = this.has([
+            'id',
+        ]);
+
+        try {
+            const id = parseInt(this.request.params.id);
+
+            SimpleItemsFacade.deleteImage(id);
+            const uploadedImageUrl = this.getUploadedFileUrl('image');
+
+            SimpleItemsFacade.updateImageUrl(id, uploadedImageUrl);
+
+            this.send(200, {
+                update: 'Success',
+                newImageUrl: uploadedImageUrl
+            });
+        }
+        catch (e) {
+            console.error('Update item image error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление количества необходимой энергии
+     */
+    updateNeedEnergy() {
+        const validate = this.has([
+            'id',
+            'energy', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const energy = parseInt(this.request.body.energy);
+
+            SimpleItemsFacade.updateNeedEnergy(id, energy);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item energy error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление количества необходимой суммы денег
+     */
+    updateNeedMoney() {
+        const validate = this.has([
+            'id',
+            'money', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const money = parseInt(this.request.body.money);
+
+            SimpleItemsFacade.updateNeedMoney(id, money);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item money error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление ID необходимого ресурса
+     */
+    updateNeedResourceId() {
+        const validate = this.has([
+            'id',
+            'newResourceId', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const newResourceId = parseInt(this.request.body.newResourceId);
+
+            SimpleItemsFacade.updateNeedResourceId(id, newResourceId);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item need resource id error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    /**
+     * Обновление количества необходимого ресурса
+     */
+    updateNeedResourceCount() {
+        const validate = this.has([
+            'id',
+            'newResourceCount', 
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const id = parseInt(this.request.params.id);
+            const newResourceCount = parseInt(this.request.body.newResourceCount);
+
+            SimpleItemsFacade.updateCountNeedResource(id, newResourceCount);
+
+            this.send(200, {
+                message: 'Update success'
+            });
+        }
+        catch (e) {
+            console.error('Update item need resource count error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+}
