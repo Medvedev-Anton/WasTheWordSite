2f4b15b463a0f8a22a21bb7733d8ef48d8dbc681
reightenfels <reightenfels@mail.ru>
2026-07-07 12:49:57 +0300

    add: users resources mapper

---
+++ backend/mappers/users_resources/users_resources_mapper.js
@@ -0,0 +1,42 @@
+import UsersResourcesMapperInterface from "./users_resources_mapper_interface.js";
+import { db } from "../../database/init.js";
+
+export default class UsersResourcesMapper extends UsersResourcesMapperInterface {
+    constructor() {
+        super();
+    }
+
+    create(userId, resourceId, count) {
+        db.prepare(`
+            INSERT INTO
+                users_resources
+            VALUES (userId, resourceId, count)    
+        `).run(userId, resourceId, count);
+    }
+
+    delete(id) {
+        db.prepare(`
+            DELETE FROM
+                users_resources
+            WHERE
+                id = ?
+        `).run(id);
+    }
+
+    findAllByUserId(userId) {
+        const result = db.prepare(`
+            SELECT
+                r.*
+            FROM
+                users_resources u
+            JOIN
+                resources r
+            ON
+                u.resourceId = r.id
+            WHERE
+                u.userId = ?
+        `).all(userId);
+
+        return result;
+    }
+}
---
+++ backend/mappers/users_resources/users_resources_mapper_interface.js
@@ -0,0 +1,33 @@
+export default class UsersResourcesMapperInterface {
+    constructor() {
+        if (new.target === 'UsersResourcesMapperInterface') {
+            throw new Error('Нельзя создать экземпляр класса UsersResourcesMapperInterface');
+        }
+    }
+
+    /**
+     * Создание записи
+     * @param {number} userId
+     * @param {number} resourceId
+     * @param {number} count
+     */
+    create(userId, resourceId, count) {
+        throw new Error('create должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление записи
+     * @param {number} id
+     */
+    delete(id) {
+        throw new Error('delete должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение всех ресурсов пользователя
+     * @param {number} userId
+     */
+    findAllByUserId(userId) {
+        throw new Error('userId должен быть переопределен в наследнике');
+    }
+}
