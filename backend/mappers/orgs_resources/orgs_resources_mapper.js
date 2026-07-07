127a854c647f997d9c221c972ae470946e307590
reightenfels <reightenfels@mail.ru>
2026-07-07 12:03:36 +0300

    add: orgs resources mapper

---
+++ backend/mappers/orgs_resources/orgs_resources_mapper.js
@@ -0,0 +1,51 @@
+import OrgsResourcesMapperInterface from "./orgs_resources_mapper_interface.js";
+import { db } from "../../database/init.js";
+
+export default class OrgsResourcesMapper extends OrgsResourcesMapperInterface {
+    constructor() {
+        super();
+    }
+
+    create(orgId, resourceId, count) {
+        db.prepare(`
+            INSERT INTO
+                orgs_resources(orgId, resourceId, count)
+            VALUES (?, ?, ?)
+        `).run(orgId, resourceId, count);
+    }
+
+    delete(id) {
+        db.prepare(`
+            DELETE FROM
+                orgs_resources
+            WHERE
+                id = ?  
+        `).run(id);
+    }
+
+    findById(id) {
+        const result = db.prepare(`
+            SELECT
+                *
+            FROM
+                orgs_resources
+            WHERE
+                id = ?    
+        `).get(id);
+
+        return result || null;
+    }
+
+    findAllByOrgId(orgId) {
+        const result = db.prepare(`
+            SELECT
+                *
+            FROM
+               orgs_resources
+            WHERE
+                orgId = ?     
+        `).all(orgId);
+
+        return result;
+    }
+}
---
+++ backend/mappers/orgs_resources/orgs_resources_mapper_interface.js
@@ -0,0 +1,41 @@
+export default class OrgsResourcesMapperInterface {
+    constructor() {
+        if (new.target === 'OrgsResourcesMapperInterface') {
+            throw new Error('Нельзя создать экземпляр класса OrgsResourcesMapperInterface');
+        }
+    }
+
+    /**
+     * Создание записи
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} count
+     */
+    create(orgId, resourceId, count) {
+        throw new Error('create должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление записи
+     * @param {number} id
+     */
+    delete(id) {
+        throw new Error('delete должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получить по ID
+     * @param {number} id
+     */
+    findById(id) {
+        throw new Error('findById должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получить все ресурсы организации
+     * @param {number} orgId
+     */
+    findAllByOrgId(orgId) {
+        throw new Error('findAllByOrgId должен быть переопределен в наследнике');
+    }
+}
