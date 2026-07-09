14cdb89a43d2923e8ce562e2dd38cc15dd90e4b0
reightenfels <reightenfels@mail.ru>
2026-07-09 17:38:19 +0300

    add: energy params mapper

---
+++ backend/mappers/energy_params/energy_params_mapper.js
@@ -0,0 +1,42 @@
+import EnergyParamsMapperInterface from "./energy_params_mapper_interface.js";
+import { db } from "../../database/init.js";
+
+export default class EnergyParamsMapper extends EnergyParamsMapperInterface {
+    constructor() {
+        super();
+    }
+
+    findByName(name) {
+        const result = db.prepare(`
+            SELECT
+                value
+            FROM
+                energy_params
+            WHERE
+                name = ?    
+        `).get(name);
+
+        if (result === undefined || result === null) {
+            return null;
+        }
+
+        const value = parseFloat(result.value);
+
+        if (isNaN(value)) {
+            return null;
+        }
+
+        return value;
+    }
+
+    update(name, newValue) {
+        db.prepare(`
+            UPDATE
+               energy_params
+            SET
+                value = ?
+            WHERE
+                name = ?     
+        `).run(newValue, name);
+    }
+}
---
+++ backend/mappers/energy_params/energy_params_mapper_interface.js
@@ -0,0 +1,24 @@
+export default class EnergyParamsMapperInterface {
+    constructor() {
+        if (new.target === 'EnergyParamsMapperInterface') {
+            throw new Error('нельзя создать экземпляр класса EnergyParamsMapperInterface');
+        }
+    }
+
+    /**
+     * Получить значение параметра
+     * @param {string} name
+     */
+    findByName(name) {
+        throw new Error('findByName должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Изменить значение параметра
+     * @param {string} name
+     * @param {float} newValue
+     */
+    update(name, newValue) {
+        throw new Error('update должен быть переопределен в наследнике');
+    }
+}
