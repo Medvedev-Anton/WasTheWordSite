2ea012c4b8f748adff2219259700fd4f3a051d3f
reightenfels <reightenfels@mail.ru>
2026-07-09 17:51:27 +0300

    add: energy params routes

--- backend/routes/energy.js
+++ backend/routes/energy.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/auth.js';
 import EnergyController from '../controllers/energy_controller.js';
+import EnergyParamsController from '../controllers/energy_params_controller.js';
 const router = express.Router();
 
+// Начисление энергии за посещение организации
 router.post('/org/:id/visit', authenticateToken, (req, res) => {
     const controller = new EnergyController(req, res);
     controller.incrementForOrgVisit();
+});
+
+// Получение значения параметра цены за покупку энергии
+router.get('/params/buyEnergyPrice', authenticateToken, (req, res) => {
+    const controller = new EnergyParamsController();
+    controller.getBuyEnergyPrice();
+});
+
+// Обновление значения параметра цены за покупку энергии
+router.post('/params/buyEnergyPrice', authenticateToken, (req, res) => {
+    const controller = new EnergyParamsController();
+    controller.updateBuyEnergyPrice();
 });
 
 export default router;
