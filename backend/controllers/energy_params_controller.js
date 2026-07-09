a12912efc63517abb12dfdc1a2d6417e6540da80
reightenfels <reightenfels@mail.ru>
2026-07-09 17:49:21 +0300

    add: energy params controller

---
+++ backend/controllers/energy_params_controller.js
@@ -0,0 +1,53 @@
+import EnergyParamsFacade from "../facades/energy_params_facade.js";
+import { MainController } from "./main_controller.js";
+
+export default class EnergyParamsController extends MainController {
+    constructor(request, response) {
+        super(request, response);
+    }
+
+    /**
+     * Вовзращает цену за покупку энергии
+     */
+    getBuyEnergyPrice() {
+        try {
+            const price = EnergyParamsFacade.getBuyEnergyPrice();
+
+            this.send(200, {
+                price: price
+            });
+        }
+        catch (e) {
+            console.error('Get buy energy price error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+
+    updateBuyEnergyPrice() {
+        const validate = this.has([
+            'newValue'
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+        
+        try {
+            const newValue = parseInt(this.request.body.newValue);
+
+            EnergyParamsFacade.updateBuyEnergyPrice(newValue);
+
+            this.send(200, {
+                message: 'success'
+            });
+        }
+        catch (e) {
+            console.error('Update buy energy price error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
+}
