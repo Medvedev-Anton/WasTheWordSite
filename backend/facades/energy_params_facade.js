af694bd094e8d2fe0d230f889464ba9b32576192
reightenfels <reightenfels@mail.ru>
2026-07-09 17:44:17 +0300

    add: energy params facade

---
+++ backend/facades/energy_params_facade.js
@@ -0,0 +1,35 @@
+import EnergyParamsMapper from "../mappers/energy_params/energy_params_mapper.js";
+import EnergyParamsService from "../services/energy_params/energy_params_service.js";
+
+export default class EnergyParamsFacade {
+    static getService() {
+        return new EnergyParamsService(
+            new EnergyParamsMapper()
+        )
+    }
+
+    /**
+     * Получить значение параметра цены покупки энергии
+     */
+    static getBuyEnergyPrice() {
+        try {
+            return this.getService().getBuyEnergyPrice();
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    /**
+     * Изменить значение параметра
+     * @param {float} newValue
+     */
+    static updateBuyEnergyPrice(newValue) {
+        try {
+            return this.getService().updateBuyEnergyPrice(newValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
