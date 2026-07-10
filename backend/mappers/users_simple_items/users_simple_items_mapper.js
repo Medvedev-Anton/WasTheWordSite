15fe533bebd330459d839ad5210a4effb551b899
reightenfels <reightenfels@mail.ru>
2026-07-10 10:06:19 +0300

    add: users simple items  mapper

---
+++ backend/mappers/users_simple_items/users_simple_items_mapper.js
@@ -0,0 +1,43 @@
+import UsersSimpleItemsMapperInterface from "./users_simple_items_mapper_interface.js";
+import { db } from "../../database/init.js";
+
+export default class UsersSimpleItemsMapper extends UsersSimpleItemsMapperInterface {
+    constructor() {
+        super();
+    }
+
+    create(userId, simpleItemId, count) {
+        db.prepare(`
+            INSERT INTO
+                users_simple_items
+            VALUES (?, ?, ?)    
+        `).run(userId, simpleItemId, count);
+    }
+
+    delete(id) {
+        db.prepare(`
+            DELETE FROM
+                users_simple_items
+            WHERE
+                id = ?
+        `).run(id);
+    }
+
+    findAllByUserId(userId) {
+        const result = db.prepare(`
+            SELECT
+                i.*,
+                u.count as count
+            FROM
+                users_simple_items u
+            JOIN
+                simpleItems i
+            ON
+                u.simpleItemId = i.id
+            WHERE
+                u.userId = ?
+        `).all(userId);
+
+        return result;
+    }
+}
---
+++ backend/mappers/users_simple_items/users_simple_items_mapper_interface.js
@@ -0,0 +1,33 @@
+export default class UsersSimpleItemsMapperInterface {
+    constructor() {
+        if (new.target === 'UsersSimpleItemsMapperInterface') {
+            throw new Error('Нельзя создать экземпляр класса UsersSimpleItemsMapperInterface');
+        }
+    }
+
+    /**
+     * Создание записи
+     * @param {number} userId
+     * @param {number} simpleItemId
+     * @param {number} count
+     */
+    create(userId, simpleItemId, count) {
+        throw new Error('create должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление записи
+     * @param {number} id
+     */
+    delete(id) {
+        throw new Error('delete должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение всех предметов пользователя
+     * @param {number} userId
+     */
+    findAllByUserId(userId) {
+        throw new Error('findAllByUserId должен быть переопределен в наследнике');
+    }
+}
