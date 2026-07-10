33e564d43c3bba906e6041e6d8992329b0db4dad
reightenfels <reightenfels@mail.ru>
2026-07-10 10:59:35 +0300

    add: workshops simple items facade

---
+++ backend/facades/workshops_simple_items_facade.js
@@ -0,0 +1,24 @@
+import WorkshopsSimpleItemsMapper from "../mappers/workshops_simple_items/workshops_simple_items_mapper.js";
+import WorkshopsSimpleItemsService from "../services/workshops_simple_items/workshops_simple_items_service.js";
+
+export default class WorkshopsSimpleItemsFacade {
+    static getService() {
+        return new WorkshopsSimpleItemsService(
+            new WorkshopsSimpleItemsMapper()
+        );
+    }
+
+    /**
+     * Создание записи
+     * @param {number} workshopId
+     * @param {number} simpleItemId
+     */
+    static create(workshopId, simpleItemId) {
+        try {
+            return this.getService().create(workshopId, simpleItemId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
