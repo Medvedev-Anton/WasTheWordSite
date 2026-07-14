98f40e702dc4c712c7021c29c22daebcebc6f9bf
reightenfels <reightenfels@mail.ru>
2026-07-14 17:29:41 +0300

    add: buy org from org simple item controller

--- backend/controllers/simple_items_controller.js
+++ backend/controllers/simple_items_controller.js
@@ -398,4 +398,51 @@ export default class SimpleItemsController extends MainController {
             });
         }
     }
+
+    /**
+     * Покупка простого предмета организацией у организации
+     */
+    buyOrgFromOrg() {
+        const validate = this.has([
+            'sellerId',
+            'buyerId',
+            'id',
+        ]);
+
+        if (validate === false) {
+            return;
+        }
+
+        try {
+            const userId = parseInt(this.request.user.userId);
+            const buyerId = parseInt(this.request.body.buyerId);
+            const buyerAdminId = OrgsFacade.getAdminId(buyerId);
+
+            if (userId !== buyerAdminId) {
+                this.send(400, {
+                    message: 'Вы не являетесь владельцем покупающей организации'
+                });
+                return;
+            }
+
+            const sellerId = parseInt(this.request.body.sellerId);
+            const resourceId = parseInt(this.request.params.id);
+
+            SimpleItemsFacade.buyOrgFromOrg(sellerId, buyerId, resourceId, 1);
+
+            const buyerBalance = BalanceFacade.entity('orgs').getBalance(buyerId);
+            const sellerResources = OrgsSimpleItemsFacade.getAllByOrgId(sellerId);
+
+            this.send(200, {
+                buyerBalance: buyerBalance,
+                sellerResources: sellerResources
+            });
+        }
+        catch (e) {
+            console.error('Buy org from org resource error:', e.message);
+            this.send(500, {
+                error: 'Server error'
+            });
+        }
+    }
 }
