168965a28398f4ffd1e81207e7db8febb078ac94
reightenfels <reightenfels@mail.ru>
2026-07-21 16:57:26 +0300

    add: orgs storages service

---
+++ backend/services/orgs_storages/orgs_storages_service.js
@@ -0,0 +1,244 @@
+import OrgsStoragesServiceInterface from "./orgs_storages_service_interface.js";
+
+export default class OrgsStoragesService extends OrgsStoragesServiceInterface {
+    constructor(mapper) {
+        super(mapper);
+    }
+
+    createContentWithResource(orgId, resourceId, count) {
+        try {
+            return this.mapper.createContentWithResource(orgId, resourceId, count);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createContentWithSimpleItem(orgId, simpleItemId, count) {
+        try {
+            return this.mapper.createContentWithSimpleItem(orgId, simpleItemId, count);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createContentWithCompoundItem(orgId, compoundItemId, count) {
+        try {
+            return this.mapper.createContentWithCompoundItem(orgId, compoundItemId, count);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    deleteContentById(id) {
+        try {
+            return this.mapper.deleteContentById(id);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findContentById(id) {
+        try {
+            return this.mapper.findContentById(id);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findAllResourcesByOrgId(orgId) {
+        try {
+            return this.mapper.findAllResourcesByOrgId(orgId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findAllSimpleItemsByOrgId(orgId) {
+        try {
+            return this.mapper.findAllSimpleItemsByOrgId(orgId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findAllCompoundItemsByOrgId(orgId) {
+        try {
+            return this.mapper.findAllCompoundItemsByOrgId(orgId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findContentByOrgAndResource(orgId, resourceId) {
+        try {
+            return this.mapper.findContentByOrgAndResource(orgId, resourceId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findContentByOrgAndSimpleItem(orgId, simpleItemId) {
+        try {
+            return this.mapper.findContentByOrgAndSimpleItem(orgId, simpleItemId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    findContentByOrgAndCompoundItem(orgId, compoundItemId) {
+        try {
+            return this.mapper.findContentByOrgAndCompoundItem(orgId, compoundItemId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    incrementOrgResource(orgId, resourceId, incrementValue) {
+        try {
+            return this.mapper.incrementOrgResource(orgId, resourceId, incrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    incrementOrgSimpleItem(orgId, simpleItemId, incrementValue) {
+        try {
+            return this.mapper.incrementOrgSimpleItem(orgId, simpleItemId, incrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    incrementOrgCompoundItem(orgId, compoundItemId, incrementValue) {
+        try {
+            return this.mapper.incrementOrgCompoundItem(orgId, compoundItemId, incrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    decrementOrgResource(orgId, resourceId, decrementValue) {
+        try {
+            return this.mapper.decrementOrgResource(orgId, resourceId, decrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    decrementOrgSimpleItem(orgId, simpleItemId, decrementValue) {
+        try {
+            return this.mapper.decrementOrgSimpleItem(orgId, simpleItemId, decrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    decrementOrgCompoundItem(orgId, compoundItemId, decrementValue) {
+        try {
+            return this.mapper.decrementOrgCompoundItem(orgId, compoundItemId, decrementValue);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    updatePriceByOrgAndResource(orgId, resourceId, newPrice) {
+        try {
+            return this.mapper.updatePriceByOrgAndResource(orgId, resourceId, newPrice);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    updatePriceByOrgAndSimpleItem(orgId, simpleItemId, newPrice) {
+        try {
+            return this.mapper.updatePriceByOrgAndSimpleItem(orgId, simpleItemId, newPrice);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+    
+    updatePriceByOrgAndCompoundItem(orgId, compoundItemId, newPrice) {
+        try {
+            return this.mapper.updatePriceByOrgAndCompoundItem(orgId, compoundItemId, newPrice);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createNewStorageMember(storageId, memberOrgId) {
+        try {
+            return this.mapper.createNewStorageMember(storageId, memberOrgId);
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createOrIncrementOrgResource(orgId, resourceId, incrementValue = 1) {
+        try {
+            const resource = this.findContentByOrgAndResource(orgId, resourceId);
+
+            if (resource === null) {
+                this.createContentWithResource(orgId, resourceId, incrementValue);
+            }
+            else {
+                this.incrementOrgResource(orgId, resourceId, incrementValue);
+            }
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createOrIncrementOrgSimpleItem(orgId, simpleItemId, incrementValue) {
+        try {
+            const simpleItem = this.findContentByOrgAndSimpleItem(orgId, simpleItemId);
+
+            if (simpleItem === null) {
+                this.createContentWithSimpleItem(orgId, simpleItemId, incrementValue);
+            }
+            else {
+                this.incrementOrgSimpleItem(orgId, simpleItemId, incrementValue);
+            }
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    createOrIncrementOrgCompoundItem(orgId, compoundItemId, incrementValue) {
+        try {
+            const compoundItem = this.findContentByOrgAndCompoundItem(orgId, compoundItemId);
+
+            if (compoundItem === null) {
+                this.createContentWithCompoundItem(orgId, compoundItemId, incrementValue);
+            }
+            else {
+                this.incrementOrgCompoundItem(orgId, compoundItemId, incrementValue);
+            }
+        }
+        catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
---
+++ backend/services/orgs_storages/orgs_storages_service_interface.js
@@ -0,0 +1,240 @@
+import OrgsStoragesMapperInterface from "../../mappers/orgs_storages/orgs_storages_mapper_interface.js";
+
+export default class OrgsStoragesServiceInterface {
+    /**
+     * @param {OrgsStoragesMapperInterface} mapper 
+     */
+    constructor(mapper) {
+        if (new.target === 'OrgsStoragesServiceInterface') {
+            throw new Error('Нельзя создать экземпляр класса OrgsStoragesServiceInterface');
+        }
+
+        this.mapper = mapper;
+    }
+
+    /**
+     * Создание записи с ресурсом
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} count
+     */
+    createContentWithResource(orgId, resourceId, count) {
+        throw new Error('createContentWithResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создание записи с простым предметом
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     * @param {number} count
+     */
+    createContentWithSimpleItem(orgId, simpleItemId, count) {
+        throw new Error('createContentWithSimpleItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создание записи с составным предметом
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     * @param {number} count
+     */
+    createContentWithCompoundItem(orgId, compoundItemId, count) {
+        throw new Error('createContentWithCompoundItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Удаление строки содержимого хранилища по id
+     * @param {number} id
+     */
+    deleteContentById(id) {
+        throw new Error('deleteContentById должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение строки содержимого хранилища по id
+     * @param {number} id
+     */
+    findContentById(id) {
+        throw new Error('findContentById должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение всех ресурсов организации по ID
+     * @param {number} orgId
+     */
+    findAllResourcesByOrgId(orgId) {
+        throw new Error('findAllResourcesByOrgId должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение всех простых предметов организации по ID
+     * @param {number} orgId
+     */
+    findAllSimpleItemsByOrgId(orgId) {
+        throw new Error('findAllSimpleItemsByOrgId должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение всех составных предметов организации по ID
+     * @param {number} orgId
+     */
+    findAllCompoundItemsByOrgId(orgId) {
+        throw new Error('findAllCompoundItemsByOrgId должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение строки содержимого по организации и ресурсу
+     * @param {number} orgId
+     * @param {number} resourceId
+     */
+    findContentByOrgAndResource(orgId, resourceId) {
+        throw new Error('findContentByOrgAndResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение строки содержимого по организации и простому предмету
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     */
+    findContentByOrgAndSimpleItem(orgId, simpleItemId) {
+        throw new Error('findContentByOrgAndSimpleItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Получение строки содержимого по организации и составному предмету
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     */
+    findContentByOrgAndCompoundItem(orgId, compoundItemId) {
+        throw new Error('findContentByOrgAndCompoundItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Инкрементировать количество ресурса у организации
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} incrementValue
+     */
+    incrementOrgResource(orgId, resourceId, incrementValue) {
+        throw new Error('incrementOrgResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Инкрементировать количество простого предмета у организации
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     * @param {number} incrementValue
+     */
+    incrementOrgSimpleItem(orgId, simpleItemId, incrementValue) {
+        throw new Error('incrementOrgSimpleItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Инкрементировать количество составного предмета у организации
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     * @param {number} incrementValue
+     */
+    incrementOrgCompoundItem(orgId, compoundItemId, incrementValue) {
+        throw new Error('incrementOrgCompoundItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Декрементировать количество ресурса у организации
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} decrementValue
+     */
+    decrementOrgResource(orgId, resourceId, decrementValue) {
+        throw new Error('decrementOrgResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Декрементировать количество простого предмета у организации
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     * @param {number} decrementValue
+     */
+    decrementOrgSimpleItem(orgId, simpleItemId, decrementValue) {
+        throw new Error('incrementOrgSimpleItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Декрементировать количество составного предмета у организации
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     * @param {number} decrementValue
+     */
+    decrementOrgCompoundItem(orgId, compoundItemId, decrementValue) {
+        throw new Error('decrementOrgCompoundItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Обновить цену по организации и ресурсу
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} newPrice
+     */
+    updatePriceByOrgAndResource(orgId, resourceId, newPrice) {
+        throw new Error('updatePriceByOrgAndResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Обновить цену по организации и простому предмету
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     * @param {number} newPrice
+     */
+    updatePriceByOrgAndSimpleItem(orgId, simpleItemId, newPrice) {
+        throw new Error('updatePriceByOrgAndSimpleItem должен быть переопределен в наследнике');
+    }
+    
+    /**
+     * Обновить цену по организации и составному предмету
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     * @param {number} newPrice
+     */
+    updatePriceByOrgAndCompoundItem(orgId, compoundItemId, newPrice) {
+        throw new Error('updatePriceByOrgAndCompoundItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создать нового члена хранилища
+     * @param {number} storageId
+     * @param {number} memberOrgId
+     */
+    createNewStorageMember(storageId, memberOrgId) {
+        throw new Error('createNewStorageMember должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создать или увеличить ресурс организации
+     * @param {number} orgId
+     * @param {number} resourceId
+     * @param {number} incrementValue
+     */
+    createOrIncrementOrgResource(orgId, resourceId, incrementValue = 1) {
+        throw new Error('createOrIncrementOrgResource должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создать или увеличить простой предмет организации
+     * @param {number} orgId
+     * @param {number} simpleItemId
+     * @param {number} incrementValue
+     */
+    createOrIncrementOrgSimpleItem(orgId, simpleItemId, incrementValue) {
+        throw new Error('createOrIncrementOrgSimpleItem должен быть переопределен в наследнике');
+    }
+
+    /**
+     * Создать или увеличить составной предмет организации
+     * @param {number} orgId
+     * @param {number} compoundItemId
+     * @param {number} incrementValue
+     */
+    createOrIncrementOrgCompoundItem(orgId, compoundItemId, incrementValue) {
+        throw new Error('createOrIncrementOrgCompoundItem должен быть переопределен в наследнике');
+    }
+}
